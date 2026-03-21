1fa1b475c1ff1de59dcf067a91175426436eea1e
Berkay Güre <bgure@hotmail.com.tr>
2026-03-21 11:15:12 +0300

    Initial Commit

---
+++ calpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calpaper
+//
+//  Created by Berkay Güre on 21.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calpaper/calpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  calpaperApp.swift
+//  calpaper
+//
+//  Created by Berkay Güre on 21.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct calpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
